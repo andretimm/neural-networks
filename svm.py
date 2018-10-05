99b8e8592951190752b52b0d59fab58f9eae4acd
Aayush Sinha <aayushsinha44@gmail.com>
2018-10-05 11:44:54 +0530

    Added SVM

---
+++ svm.py
@@ -0,0 +1,22 @@
+import numpy as np
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.preprocessing import StandardScaler
+
+# Importing dataset
+X, y = load_breast_cancer(return_X_y=True)
+
+# Scaling data
+scaler = StandardScaler()
+X = scaler.fit_transform(X)
+
+# Train test split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+# Training
+clf = SVC()
+clf.fit(X_train, y_train)
+acc = clf.score(X_test, y_test)
+
+print('Accuracy:', acc)
